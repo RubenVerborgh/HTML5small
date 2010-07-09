b7202ee30a71b6d247158d0edebe047938a585d5
Run Paint Run Run <runrun@runpaint.org>
2010-07-09 20:45:07 +0100

    Minor refactoring

--- lib/h5-min.rb
+++ lib/h5-min.rb
@@ -24,12 +24,13 @@ module HTML5
       OptionalTags.remove minifier.buf.strip
     end
 
-    attr_accessor :in_pre, :buf, :text_node
+    attr_accessor :in_pre, :buf, :text_node, :entities
 
     def initialize
       @in_pre = false
       @buf, @text_node = '', ''
       @stack = []
+      @entities = HTMLEntities.new :expanded
     end
 
     # HTML5 documents begin with the doctype
@@ -38,7 +39,7 @@ module HTML5
     end
 
     def start_element name, attrs = []
-      name = normalise_tag_name name
+      name = normalise_name name
       dump_text_node
       @stack.push name
       self.in_pre = true if PRE_TAGS.include?(name)
@@ -46,70 +47,60 @@ module HTML5
     end
 
     def end_element name
-      name = normalise_tag_name name
+      name = normalise_name name
       dump_text_node
       @stack.pop
       buf.rstrip! unless in_pre
-      self.in_pre = false if PRE_TAGS.include?(name)
+      self.in_pre = PRE_TAGS.include?(name)
       buf << "</#{name}>"
     end
 
-    def comment(string)
+    def comment string
       # I.E "conditional comments" should be retained as-is
       if string =~ /\[if\s+lt\s+IE\s+\d+\]/i
         buf << "<!--#{string}-->"
       end
     end
 
-    def cdata_block(string)
+    def cdata_block string
       buf << string
     end
 
-    def characters(chars)
+    def characters chars
       text_node << chars
     end
 
     private
-    def format_attribute_value(value)
+    def format_attribute_value value
       value = format_entities value
       value_needs_quoting?(value) ? %Q{"#{value}"} : value
     end
 
-    def normalise_attribute_name name
+    def normalise_name name
       name.downcase.to_sym
     end
 
     def format_attributes attrs
       return '' if attrs.empty?
       Hash[*attrs].map do |name, value|
-        [normalise_attribute_name(name), format_attribute_value(value)]
+        [normalise_name(name), format_attribute_value(value)]
       end.sort_by do |name, value|
         name
       end.map do |name, value|
         "#{name}=#{value}"
       end.join(' ').insert(0, ' ')
     end
-
-    def normalise_tag_name tag
-      tag.downcase.to_sym
-    end
     
     # Can the given value be legally unquoted as per
     # http://www.whatwg.org/specs/web-apps/current-work/multipage/syntax.html#attributes-0
     # ?
     def value_needs_quoting? value
-      # must not contain any literal space characters
-      return true if value =~ /[[:space:]]/
-      # must not contain any """, "'", ">", "=", characters
-      return true if value =~ /["'><=`]/
-      # must not be the empty string
-      return true if value == ''
-      false
+      # must not contain any " ", """, "'", ">", or "=", characters
+      value =~ /[[:space:]"'><=`]/ or value.empty?
     end
 
     def format_entities html
-      he = HTMLEntities.new(:expanded)
-      he.encode(he.decode(html), :basic)
+      entities.encode(entities.decode(html), :basic)
     end
 
     def format_text_node
